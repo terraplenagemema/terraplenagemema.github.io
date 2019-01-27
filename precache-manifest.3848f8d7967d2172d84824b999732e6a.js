b6802cc10c5440e117d5f62482d5cc8440ced405
Edevaldo <edevaldomalaquias@gmail.com>
2019-01-27 19:26:34 +0000

    Release 20190127_172710

---
+++ precache-manifest.3848f8d7967d2172d84824b999732e6a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "628b70ca0758078bd2b6cd76e1e36a99",
+    "url": "/static/media/c4.628b70ca.jpg"
+  },
+  {
+    "revision": "1e94b959b1d4faee5078",
+    "url": "/static/css/main.629ea998.chunk.css"
+  },
+  {
+    "revision": "7db9279d59c66ab8b7fc",
+    "url": "/static/js/1.7db9279d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3fff84308c2b9e3ae8ea68f30454768b",
+    "url": "/static/media/c2.3fff8430.jpg"
+  },
+  {
+    "revision": "1e94b959b1d4faee5078",
+    "url": "/static/js/main.1e94b959.chunk.js"
+  },
+  {
+    "revision": "c8a30c90fb2e6465e8181968eb8727d5",
+    "url": "/static/media/company.c8a30c90.jpg"
+  },
+  {
+    "revision": "3abe0fcd5adcb8bcc944d5f9e8d37f0b",
+    "url": "/static/media/c1.3abe0fcd.jpg"
+  },
+  {
+    "revision": "639813c10fc4848a01fdf9f68fa496e3",
+    "url": "/static/media/c3.639813c1.jpg"
+  },
+  {
+    "revision": "d2fda8d9a1231b2bb92157957bad5955",
+    "url": "/static/media/bg.d2fda8d9.jpg"
+  },
+  {
+    "revision": "a6d3a3c6ba3b120853af28c3dcb69549",
+    "url": "/index.html"
+  }
+];
