5b024bb97a86a472d936b69c2a861f1f2a8bc41c
Edevaldo <edevaldomalaquias@gmail.com>
2019-02-16 23:44:22 -0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+echo "==> Building"
+git stash
+git checkout src
+git reset --hard src --
+npm run build
+
+echo "==> Copying build to master branch"
+git checkout master
+find . -maxdepth 1 \
+    -not -name build \
+    -not -name . \
+    -not -name deploy.sh \
+    -not -name node_modules \
+    -not -name .git \
+    -not -name .gitignore \
+    -print0 | xargs -0 rm -rf --
+mv build/* .
+rm -rf build
+
+echo "==> Committing changes (if necessary)"
+echo "deploy.sh" > .gitignore
+echo "node_modules" >> .gitignore
+git add .gitignore
+git add .
+git reset HEAD -- .gitignore
+rm -rf .gitignore
+export TZ='America/Sao_Paulo'
+release=`date +"%Y%m%d_%H%M%S"`
+git commit -m "Release $release"
+if [ $? == 1 ]; then
+    echo "==> Nothing changed, new release is unnecessary"
+else
+    echo "==> Pushing release $release"
+    git push origin master
+fi;
+
+echo "==> Restoring development environment"
+git checkout src
+git reset --hard src --
+git stash pop
